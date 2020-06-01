307f831949f8ebd4311292a22ea9e778128cc35f
Korran <korranhorn009@gmail.com>
2020-06-01 10:51:01 +0300

    example value animation

--- app/src/main/java/ru/mertsalovda/animation/MainActivity.kt
+++ app/src/main/java/ru/mertsalovda/animation/MainActivity.kt
@@ -21,22 +21,23 @@ class MainActivity : AppCompatActivity() {
         onAnimatorSet(view)
     }
 
+    private val duration = 1000L
+
     fun onValueAnimator(view: View) {
-        val va = ValueAnimator.ofFloat(1f, 3f, 1f)
-        val duration = 1000L
-        va.duration = duration
-        va.addUpdateListener {
-            view.scaleX = it.animatedValue as Float
-            view.scaleY = it.animatedValue as Float
+        ValueAnimator.ofFloat(1f, 3f, 1f).apply {
+            this.duration = duration
+            addUpdateListener {
+                view.scaleX = it.animatedValue as Float
+                view.scaleY = it.animatedValue as Float
+            }
+            repeatCount = 1
+            start()
         }
-        va.repeatCount = 1
-        va.start()
     }
 
     fun onObjectAnimator(view: View) {
         val oaScaleX = ObjectAnimator.ofFloat(view, View.SCALE_X, 1f, 3f, 1f)
         val oaScaleY = ObjectAnimator.ofFloat(view, View.SCALE_Y, 1f, 3f, 1f)
-        val duration = 1000L
         oaScaleX.duration = duration
         oaScaleY.duration = duration
 
@@ -51,11 +52,12 @@ class MainActivity : AppCompatActivity() {
         val pvhX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, 3f, 1f)
         val pvhY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, 3f, 1f)
 
-        val oa = ObjectAnimator.ofPropertyValuesHolder(view, pvhX, pvhY)
+        ObjectAnimator.ofPropertyValuesHolder(view, pvhX, pvhY).apply {
+            this.duration = duration
+            repeatCount = 1
+            start()
+        }
 
-        oa.duration = 1000L
-        oa.repeatCount = 1
-        oa.start()
     }
 
     private fun onAnimatorSet(view: View) {
@@ -63,15 +65,14 @@ class MainActivity : AppCompatActivity() {
         val oaScaleY = ObjectAnimator.ofFloat(view, View.SCALE_Y, 0f, 2f)
         val oaTranslationX = ObjectAnimator.ofFloat(view, View.TRANSLATION_X, 0f, 100f, -100f, 0f)
 
-        val duration = 1000L
-
         oaScaleX.duration = duration
         oaScaleY.duration = duration
         oaTranslationX.duration = duration
 
-        val animatorSet = AnimatorSet()
+        AnimatorSet().apply {
+            play(oaScaleX).with(oaScaleY).before(oaTranslationX)
+            start()
+        }
 
-        animatorSet.play(oaScaleX).with(oaScaleY).before(oaTranslationX)
-        animatorSet.start()
     }
 }

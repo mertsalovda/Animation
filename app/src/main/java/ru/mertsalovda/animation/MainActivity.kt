e3887c94bc7202df87165aa36d26abcb48618496
Korran <korranhorn009@gmail.com>
2020-06-04 19:33:18 +0300

    example animation drawable

--- app/src/main/java/ru/mertsalovda/animation/MainActivity.kt
+++ app/src/main/java/ru/mertsalovda/animation/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.mertsalovda.animation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Animatable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.ac_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ac_main)
+
+        imageView.setOnClickListener {
+            (imageView.drawable as Animatable).start()
+        }
     }
 }

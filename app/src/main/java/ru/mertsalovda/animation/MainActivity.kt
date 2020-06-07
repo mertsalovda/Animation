fe999e1f9d7f1956c8dbf7682c0087d51eec2b2e
Korran <korranhorn009@gmail.com>
2020-06-07 03:15:10 +0300

    +1) На экране есть кнопка. При нажатии её, она убегает в другой угол, но не исчезает. +2) Кнопка может пробежать полный круг по экрану. +3) (Дополнительно) Каждый четный круг на экране появляется сообщение об этом.

--- app/src/main/java/ru/mertsalovda/animation/MainActivity.kt
+++ app/src/main/java/ru/mertsalovda/animation/MainActivity.kt
@@ -3,36 +3,41 @@ package ru.mertsalovda.animation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.*
-import android.util.TypedValue
+import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.FrameLayout
-import android.widget.TextView
-import java.util.*
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val scenes = listOf(R.layout.scene2, R.layout.scene3, R.layout.scene4, R.layout.scene1)
+    private var posotion = 0
+    private var count = 1
+    private lateinit var scene: Scene
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ac_main)
+    }
 
+    fun onClick(view: View) {
         val sceneRoot = findViewById<FrameLayout>(R.id.container)
-        val animatedObject = findViewById<TextView>(R.id.animatedObject)
-        animatedObject.setOnClickListener {
-            val random = Random()
-            TransitionManager.beginDelayedTransition(sceneRoot)
-            animatedObject.setTextSize(TypedValue.COMPLEX_UNIT_SP, random.nextInt(17) + 16f)
+        scene = Scene.getSceneForLayout(sceneRoot, scenes[posotion], baseContext)
+        TransitionSet().apply {
+            addTransition(ChangeBounds())
+            ordering = TransitionSet.ORDERING_SEQUENTIAL
+            duration = 500L
+            interpolator = AccelerateDecelerateInterpolator()
+            TransitionManager.go(scene, this)
         }
-        animatedObject.setOnLongClickListener {
-            val scene = Scene.getSceneForLayout(sceneRoot, R.layout.scene2, baseContext)
-            TransitionSet().apply {
-                addTransition(Fade(Fade.OUT))
-                addTransition(ChangeBounds())
-                addTransition(Fade(Fade.IN))
-                ordering = TransitionSet.ORDERING_SEQUENTIAL
-                duration = 500L
-                interpolator = AccelerateDecelerateInterpolator()
-                TransitionManager.go(scene, this)
+        if (posotion == scenes.size - 1) {
+            posotion = 0
+            if (count % 2 == 0) {
+                Toast.makeText(this, "Чётный круг: №$count", Toast.LENGTH_SHORT).show()
             }
-            true
+            count++
+        } else {
+            posotion++
         }
     }
 }
